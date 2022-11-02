75cd4ad958e99038d3f6d9bd5cf6fa5c1b405b08
Dan Green <danngreen1@gmail.com>
2022-11-01 22:24:45 -0700

    Added OR and val for strong enums

--- util/bitfield.hh
+++ util/bitfield.hh
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstdint>
+#include <initializer_list>
 #include <type_traits>
 
 template<typename T>
@@ -9,4 +10,19 @@ auto constexpr bitfield(T const val) {
 template<typename... Args>
 auto constexpr bitfield(const Args... args) {
 	return (... | bitfield(args));
+}
+
+template<typename Enum>
+constexpr std::underlying_type_t<Enum> OR(std::initializer_list<Enum> fields) {
+	using UT = std::underlying_type_t<Enum>;
+	UT val;
+	for (auto el : fields) {
+		val |= static_cast<UT>(el);
+	}
+	return val;
+}
+
+template<typename Enum>
+constexpr auto val(Enum field) {
+	return static_cast<std::underlying_type_t<Enum>>(field);
 }
