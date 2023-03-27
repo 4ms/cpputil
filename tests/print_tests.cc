aeea7dde74ddd5eec4b5fe07ea9701dac1679044
Lennart <Lennart@binarylabs.dev>
2023-03-27 16:41:48 +0200

    fix lambdas as template paramters causing "no binding" error

--- tests/print_tests.cc
+++ tests/print_tests.cc
@@ -194,12 +194,16 @@ namespace
 unsigned num_chars_logged = 0;
 unsigned num_chars_warned = 0;
 
+auto CountPrintedFunc = [](auto) { num_chars_logged++; };
+auto CountWarnedFunc  = [](auto) { num_chars_warned++; };
+
 void log(auto... x) {
-	(print<[](const char c) { num_chars_logged++; }>(x), ...);
+	
+	(print<CountPrintedFunc>(x), ...);
 }
 
 void warn(auto... x) {
-	(print<[](const char c) { num_chars_warned++; }>(x), ...);
+	(print<CountWarnedFunc>(x), ...);
 }
 } // namespace
 
