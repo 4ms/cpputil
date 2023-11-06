55483c4091307e65290d48915b8cd9b6f264b575
Dan Green <danngreen1@gmail.com>
2023-11-06 15:24:33 -0800

    Color565 type

--- util/colors.hh
+++ util/colors.hh
@@ -21,6 +21,50 @@ static inline uint32_t __USAT(int32_t val, uint32_t sat) {
 #define builtin_add_u8(x, y) ((x) + (y))
 #endif
 
+struct Color565 {
+	uint16_t r : 5;
+	uint16_t g : 6;
+	uint16_t b : 5;
+
+	constexpr Color565(uint8_t red, uint8_t green, uint8_t blue) {
+		r = red >> 3;
+		g = green >> 2;
+		b = blue >> 3;
+	}
+	constexpr operator uint16_t() const {
+		return *this;
+	}
+	constexpr uint16_t raw() const {
+		return (r << 11) | (g << 5) | b;
+	}
+	//0..255
+	constexpr uint8_t red() const {
+		return (raw() & 0xf800) >> 8;
+	}
+	//0..255
+	constexpr uint8_t green() {
+		return (raw() & 0x07e0) >> 3;
+	}
+	//0..255
+	constexpr uint8_t blue() {
+		return (raw() & 0x001f) << 3;
+	}
+};
+
+static_assert(sizeof(Color565) == 2);
+static_assert(Color565{0xFF, 0x00, 0x00}.red() == (0xFF & 0xF8));
+static_assert(Color565{0xAA, 0xFF, 0xFF}.red() == (0xAA & 0xF8));
+static_assert(Color565{0x55, 0xFF, 0xFF}.red() == (0x55 & 0xF8));
+static_assert(Color565{0x00, 0xFF, 0x00}.green() == (0xFF & 0xFC));
+static_assert(Color565{0xFF, 0xAA, 0xFF}.green() == (0xAA & 0xFC));
+static_assert(Color565{0xFF, 0x55, 0xFF}.green() == (0x55 & 0xFC));
+static_assert(Color565{0x00, 0x00, 0xFF}.blue() == (0xFF & 0xF8));
+static_assert(Color565{0xFF, 0xFF, 0xAA}.blue() == (0xAA & 0xF8));
+static_assert(Color565{0xFF, 0xFF, 0x55}.blue() == (0x55 & 0xF8));
+static_assert(Color565{0xFF, 0xFF, 0xFF}.raw() == 0xFFFF);
+static_assert(Color565{0x80, 0x80, 0x80}.raw() == 0x8410);
+static_assert(Color565{0x00, 0x00, 0x00}.raw() == 0x0000);
+
 struct Color {
 	// Todo: measure how much slower it is to use floats for Adjustment.r/b/g
 	struct Adjustment {
