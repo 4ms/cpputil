0277f759cce65aef53f347fbcf5f8dce960ac9a6
Dan Green <danngreen1@gmail.com>
2023-11-14 21:03:58 -0800

    Put RGB565 colors in own file

--- util/colors.hh
+++ util/colors.hh
@@ -21,61 +21,6 @@ static inline uint32_t __USAT(int32_t val, uint32_t sat) {
 #define builtin_add_u8(x, y) ((x) + (y))
 #endif
 
-struct Color565 {
-	uint16_t r : 5;
-	uint16_t g : 6;
-	uint16_t b : 5;
-
-	constexpr Color565(uint8_t red, uint8_t green, uint8_t blue) {
-		r = red >> 3;
-		g = green >> 2;
-		b = blue >> 3;
-	}
-	constexpr uint16_t raw() const {
-		return (r << 11) | (g << 5) | b;
-	}
-	//0..255
-	constexpr uint8_t red() const {
-		return (raw() & 0xf800) >> 8;
-	}
-	//0..255
-	constexpr uint8_t green() {
-		return (raw() & 0x07e0) >> 3;
-	}
-	//0..255
-	constexpr uint8_t blue() {
-		return (raw() & 0x001f) << 3;
-	}
-};
-namespace Colors565
-{
-static constexpr auto Red = Color565{0xFF, 0x00, 0x00};
-static constexpr auto Orange = Color565{0xFF, 0xAA, 0x00};
-static constexpr auto Yellow = Color565{0xFF, 0xFF, 0x00};
-static constexpr auto Green = Color565{0x00, 0xFF, 0x00};
-static constexpr auto Blue = Color565{0x00, 0x00, 0xFF};
-static constexpr auto Cyan = Color565{0x00, 0xFF, 0xFF};
-static constexpr auto Purple = Color565{0x88, 0x00, 0xFF};
-static constexpr auto Pink = Color565{0xFF, 0x00, 0xAA};
-static constexpr auto White = Color565{0xFF, 0xFF, 0xFF};
-static constexpr auto Black = Color565{0x00, 0x00, 0x00};
-static constexpr auto Grey = Color565{0x80, 0x80, 0x80};
-} // namespace Colors565
-
-static_assert(sizeof(Color565) == 2);
-static_assert(Color565{0xFF, 0x00, 0x00}.red() == (0xFF & 0xF8));
-static_assert(Color565{0xAA, 0xFF, 0xFF}.red() == (0xAA & 0xF8));
-static_assert(Color565{0x55, 0xFF, 0xFF}.red() == (0x55 & 0xF8));
-static_assert(Color565{0x00, 0xFF, 0x00}.green() == (0xFF & 0xFC));
-static_assert(Color565{0xFF, 0xAA, 0xFF}.green() == (0xAA & 0xFC));
-static_assert(Color565{0xFF, 0x55, 0xFF}.green() == (0x55 & 0xFC));
-static_assert(Color565{0x00, 0x00, 0xFF}.blue() == (0xFF & 0xF8));
-static_assert(Color565{0xFF, 0xFF, 0xAA}.blue() == (0xAA & 0xF8));
-static_assert(Color565{0xFF, 0xFF, 0x55}.blue() == (0x55 & 0xF8));
-static_assert(Color565{0xFF, 0xFF, 0xFF}.raw() == 0xFFFF);
-static_assert(Color565{0x80, 0x80, 0x80}.raw() == 0x8410);
-static_assert(Color565{0x00, 0x00, 0x00}.raw() == 0x0000);
-
 struct Color {
 	// Todo: measure how much slower it is to use floats for Adjustment.r/b/g
 	struct Adjustment {
--- util/colors_rgb565.hh
+++ util/colors_rgb565.hh
@@ -1,27 +1,35 @@
 #pragma once
 #include <cstdint>
 
-struct Color565 {
+struct RGB565 {
 	uint16_t r : 5;
 	uint16_t g : 6;
 	uint16_t b : 5;
 
-	constexpr Color565(uint8_t red, uint8_t green, uint8_t blue) {
+	constexpr RGB565(uint8_t red, uint8_t green, uint8_t blue) {
 		r = red >> 3;
 		g = green >> 2;
 		b = blue >> 3;
 	}
+
 	constexpr uint16_t raw() const {
 		return (r << 11) | (g << 5) | b;
 	}
+
+	constexpr operator uint16_t() const {
+		return raw();
+	}
+
 	//0..255
 	constexpr uint8_t red() const {
 		return (raw() & 0xf800) >> 8;
 	}
+
 	//0..255
 	constexpr uint8_t green() {
 		return (raw() & 0x07e0) >> 3;
 	}
+
 	//0..255
 	constexpr uint8_t blue() {
 		return (raw() & 0x001f) << 3;
@@ -29,29 +37,29 @@ struct Color565 {
 };
 namespace Colors565
 {
-static constexpr auto Red = Color565{0xFF, 0x00, 0x00};
-static constexpr auto Orange = Color565{0xFF, 0xAA, 0x00};
-static constexpr auto Yellow = Color565{0xFF, 0xFF, 0x00};
-static constexpr auto Green = Color565{0x00, 0xFF, 0x00};
-static constexpr auto Blue = Color565{0x00, 0x00, 0xFF};
-static constexpr auto Cyan = Color565{0x00, 0xFF, 0xFF};
-static constexpr auto Purple = Color565{0x88, 0x00, 0xFF};
-static constexpr auto Pink = Color565{0xFF, 0x00, 0xAA};
-static constexpr auto White = Color565{0xFF, 0xFF, 0xFF};
-static constexpr auto Black = Color565{0x00, 0x00, 0x00};
-static constexpr auto Grey = Color565{0x80, 0x80, 0x80};
+static constexpr auto Red = RGB565{0xFF, 0x00, 0x00};
+static constexpr auto Orange = RGB565{0xFF, 0xAA, 0x00};
+static constexpr auto Yellow = RGB565{0xFF, 0xFF, 0x00};
+static constexpr auto Green = RGB565{0x00, 0xFF, 0x00};
+static constexpr auto Blue = RGB565{0x00, 0x00, 0xFF};
+static constexpr auto Cyan = RGB565{0x00, 0xFF, 0xFF};
+static constexpr auto Purple = RGB565{0x88, 0x00, 0xFF};
+static constexpr auto Pink = RGB565{0xFF, 0x00, 0xAA};
+static constexpr auto White = RGB565{0xFF, 0xFF, 0xFF};
+static constexpr auto Black = RGB565{0x00, 0x00, 0x00};
+static constexpr auto Grey = RGB565{0x80, 0x80, 0x80};
 } // namespace Colors565
 
-static_assert(sizeof(Color565) == 2);
-static_assert(Color565{0xFF, 0x00, 0x00}.red() == (0xFF & 0xF8));
-static_assert(Color565{0xAA, 0xFF, 0xFF}.red() == (0xAA & 0xF8));
-static_assert(Color565{0x55, 0xFF, 0xFF}.red() == (0x55 & 0xF8));
-static_assert(Color565{0x00, 0xFF, 0x00}.green() == (0xFF & 0xFC));
-static_assert(Color565{0xFF, 0xAA, 0xFF}.green() == (0xAA & 0xFC));
-static_assert(Color565{0xFF, 0x55, 0xFF}.green() == (0x55 & 0xFC));
-static_assert(Color565{0x00, 0x00, 0xFF}.blue() == (0xFF & 0xF8));
-static_assert(Color565{0xFF, 0xFF, 0xAA}.blue() == (0xAA & 0xF8));
-static_assert(Color565{0xFF, 0xFF, 0x55}.blue() == (0x55 & 0xF8));
-static_assert(Color565{0xFF, 0xFF, 0xFF}.raw() == 0xFFFF);
-static_assert(Color565{0x80, 0x80, 0x80}.raw() == 0x8410);
-static_assert(Color565{0x00, 0x00, 0x00}.raw() == 0x0000);
+static_assert(sizeof(RGB565) == 2);
+static_assert(RGB565{0xFF, 0x00, 0x00}.red() == (0xFF & 0xF8));
+static_assert(RGB565{0xAA, 0xFF, 0xFF}.red() == (0xAA & 0xF8));
+static_assert(RGB565{0x55, 0xFF, 0xFF}.red() == (0x55 & 0xF8));
+static_assert(RGB565{0x00, 0xFF, 0x00}.green() == (0xFF & 0xFC));
+static_assert(RGB565{0xFF, 0xAA, 0xFF}.green() == (0xAA & 0xFC));
+static_assert(RGB565{0xFF, 0x55, 0xFF}.green() == (0x55 & 0xFC));
+static_assert(RGB565{0x00, 0x00, 0xFF}.blue() == (0xFF & 0xF8));
+static_assert(RGB565{0xFF, 0xFF, 0xAA}.blue() == (0xAA & 0xF8));
+static_assert(RGB565{0xFF, 0xFF, 0x55}.blue() == (0x55 & 0xF8));
+static_assert(RGB565{0xFF, 0xFF, 0xFF}.raw() == 0xFFFF);
+static_assert(RGB565{0x80, 0x80, 0x80}.raw() == 0x8410);
+static_assert(RGB565{0x00, 0x00, 0x00}.raw() == 0x0000);
