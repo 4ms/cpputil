54b3df1010a89502cd03faebfadcdedad4e6e541
Dan Green <danngreen1@gmail.com>
2024-06-17 15:33:11 -0700

    AnalyzedSignal can be a class with a constructor for the IIR coef

--- util/analyzed_signal.hh
+++ util/analyzed_signal.hh
@@ -2,10 +2,10 @@
 #include <cstdint>
 #include <limits>
 
-template<uint32_t iir_coef_int>
-struct AnalyzedSignal {
-	static constexpr float iir_coef = 1.f / (float)iir_coef_int;
-	static constexpr float iir_coef_inv = 1.f - iir_coef;
+struct AnalyzedSig {
+	AnalyzedSig(float iircoef)
+		: iir_coef{iircoef} {
+	}
 
 	float min = std::numeric_limits<float>::max();
 	float max = std::numeric_limits<float>::min();
@@ -26,6 +26,16 @@ struct AnalyzedSignal {
 		iir = curval;
 		min = curval; // std::numeric_limits<float>::max();
 		max = curval; // std::numeric_limits<float>::min();
+	}
+
+private:
+	float iir_coef;
+};
+
+template<uint32_t iir_coef_int>
+struct AnalyzedSignal : AnalyzedSig {
+	AnalyzedSignal()
+		: AnalyzedSig{1.f / (float)iir_coef_int} {
 	}
 };
 
