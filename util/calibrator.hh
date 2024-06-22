30376815d71432849958021318a2ff26420cf42a
Dan Green <danngreen1@gmail.com>
2024-06-21 22:28:25 -0700

    Calibrator can perform inverse function

--- util/calibrator.hh
+++ util/calibrator.hh
@@ -1,5 +1,5 @@
 #pragma once
-#include <cmath>
+#include <utility>
 
 class Calibrator {
 public:
@@ -17,6 +17,10 @@ public:
 		return (raw - _offset) * _slope;
 	}
 
+	constexpr float reverse_calibrate(float val) {
+		return val / _slope + _offset;
+	}
+
 	void set_slope_offset(float slope, float offset) {
 		_slope = slope;
 		_offset = offset;
@@ -27,6 +31,9 @@ public:
 		_offset = low_measurement - (target_low / _slope);
 	}
 
+	// calibrate_chan(TARGET, MEASUREMENT)
+	// outputs cal: when the MCU writes TARGET 24-bit value to the codec DAC, the user sees MEASUREMENT volts on the jack.
+	// inputs cal: when the user inputs TARGET volts to the jack, the codec reads MEASUREMENT 24-bit value.
 	constexpr void calibrate_chan(std::pair<float, float> target, std::pair<float, float> measurement) {
 		calibrate_chan(target.first, target.second, measurement.first, measurement.second);
 	}
@@ -43,3 +50,6 @@ private:
 	float _slope = 1.0f;
 	float _offset = 0.f;
 };
+
+static_assert(Calibrator{1.05f, -.003f}.reverse_calibrate(Calibrator{1.05f, -.003f}.adjust(9.32f)) == 9.32f);
+static_assert(Calibrator{1.05f, -.003f}.adjust(Calibrator{1.05f, -.003f}.reverse_calibrate(9.32f)) == 9.32f);
