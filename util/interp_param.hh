ae2f9f4b98a9d64c272891aff32e21113e343abb
Dan Green <danngreen1@gmail.com>
2024-07-16 16:52:50 -0700

    Add InterpParam with variable step size

--- util/interp_param.hh
+++ util/interp_param.hh
@@ -12,10 +12,12 @@ struct InterpParam {
 		T d = new_val - cur_val;
 		step_size = d / num_updates;
 	}
+
 	T next() {
 		cur_val += step_size;
 		return cur_val;
 	}
+
 	T get_step_size() {
 		return step_size;
 	}
@@ -24,4 +26,37 @@ struct InterpParam {
 
 private:
 	T step_size;
+};
+
+template<typename T>
+struct InterpParamVariable {
+	InterpParamVariable(unsigned num_updates = 256)
+		: cur_val{0}
+		, step_size{0}
+		, num_updates{num_updates} {
+	}
+
+	void set_new_value(T new_val) {
+		T d = new_val - cur_val;
+		step_size = d / T(num_updates);
+	}
+
+	T next() {
+		cur_val += step_size;
+		return cur_val;
+	}
+
+	T get_step_size() {
+		return step_size;
+	}
+
+	void set_num_updates(unsigned num_updates) {
+		this->num_updates = num_updates;
+	}
+
+	T cur_val;
+
+private:
+	T step_size;
+	unsigned num_updates;
 };
