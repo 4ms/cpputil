a755e4c8628528f510ba384d833be12350317ce4
Dan Green <danngreen1@gmail.com>
2024-08-11 22:22:22 -0700

    Monotonic Allocator can report size/free

--- util/monotonic_allocator.hh
+++ util/monotonic_allocator.hh
@@ -24,6 +24,14 @@ public:
 		offset = 0;
 	}
 
+	size_t free_space() {
+		return arena.size() - offset;
+	}
+
+	size_t total_size() {
+		return arena.size();
+	}
+
 private:
 	std::span<uint8_t> arena;
 	std::size_t offset{};
