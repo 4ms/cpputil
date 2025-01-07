d718863c3fa692a83d61edde1654a9322e71738c
Dan Green <danngreen1@gmail.com>
2025-01-06 16:33:08 -0800

    Partition Two sorts

--- util/partition.hh
+++ util/partition.hh
@@ -9,11 +9,23 @@ struct PartitionTwo {
 	FixedVector<unsigned, MaxElements> b{};
 
 	PartitionTwo(std::span<unsigned> vals) {
+		struct IdVal {
+			unsigned val;
+			unsigned id;
+		};
+
+		FixedVector<IdVal, MaxElements> ordered;
+		for (auto val : vals) {
+			ordered.push_back({val, (unsigned)ordered.size()});
+		}
+
+		std::ranges::sort(ordered, std::greater{}, &IdVal::val);
+
 		unsigned a_sum = 0;
 		unsigned b_sum = 0;
 
 		// The part with the smaller sum gets the next element
-		for (auto i = 0u; auto val : vals) {
+		for (auto [val, i] : ordered) {
 			if (a_sum <= b_sum) {
 				a_sum += val;
 				a.push_back(i);
@@ -21,7 +33,6 @@ struct PartitionTwo {
 				b_sum += val;
 				b.push_back(i);
 			}
-			i++;
 		}
 	}
 };
