119e594ad1ed587d04dcaff92f4ef245b91855eb
Dan Green <danngreen1@gmail.com>
2025-01-17 22:17:29 -0800

    Fix PollChange breaking with uint64_t

--- util/poll_change.hh
+++ util/poll_change.hh
@@ -4,7 +4,7 @@
 // But only checks at regular periods of `update_ticks`
 class PollChange {
 public:
-	PollChange(int update_ticks)
+	PollChange(unsigned update_ticks)
 		: update_ticks{update_ticks} {
 	}
 
@@ -12,7 +12,7 @@ public:
 		change_detected = true;
 	}
 
-	void poll(int now, bool state) {
+	void poll(unsigned now, bool state) {
 		if (now - last_update_time >= update_ticks) {
 			last_update_time = now;
 
@@ -25,7 +25,7 @@ public:
 
 	// Defer calling state_func() until poll time has expired.
 	// Useful if checking the state is a heavy operation.
-	void poll(int now, auto state_func) {
+	void poll(unsigned now, auto state_func) {
 		if (now - last_update_time >= update_ticks) {
 			last_update_time = now;
 
@@ -48,6 +48,6 @@ public:
 private:
 	bool current_state = false;
 	bool change_detected = false;
-	int last_update_time = 0;
-	int update_ticks;
+	unsigned last_update_time = 0;
+	unsigned update_ticks;
 };
