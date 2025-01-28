834637a4d70c272efb9eba6ae9ee3af83a71d8dd
Dan Green <danngreen1@gmail.com>
2025-01-27 23:12:48 -0800

    Add tokenize_string

--- util/string_util.hh
+++ util/string_util.hh
@@ -1,5 +1,6 @@
 #pragma once
 #include <string>
+#include <vector>
 
 struct ReplaceString {
 	std::string str;
@@ -21,3 +22,22 @@ struct ReplaceString {
 		return *this;
 	}
 };
+
+inline void
+tokenize_string(std::string_view str, std::vector<std::string> &tokens, std::string const &delimiters = ",") {
+	size_t lastPos = str.find_first_not_of(delimiters, 0);
+
+	// Find first non-delimiter.
+	std::string::size_type pos = str.find_first_of(delimiters, lastPos);
+
+	while (pos != std::string::npos || lastPos != std::string::npos) {
+		// Found a token, add it to the vector.
+		tokens.push_back(std::string(str.substr(lastPos, pos - lastPos)));
+
+		// Skip delimiters.
+		lastPos = str.find_first_not_of(delimiters, pos);
+
+		// Find next non-delimiter.
+		pos = str.find_first_of(delimiters, lastPos);
+	}
+}
