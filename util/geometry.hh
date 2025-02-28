0ed1cdf0c501aa7a6cd0ed43e842baf1468cc4ff
Dan Green <danngreen1@gmail.com>
2025-02-28 10:34:31 -0800

    Add check for concave polygon

--- util/geometry.hh
+++ util/geometry.hh
@@ -1,5 +1,6 @@
 #pragma once
 #include <cstdint>
+#include <span>
 
 struct Rect;
 
@@ -63,3 +64,49 @@ struct Rect {
 // {
 // 	return RectC{.left = left, .top = top, .right = right(), .bottom = bottom()};
 // }
+
+template<typename T>
+constexpr bool is_poly_concave(std::span<const T> points) requires requires(T pt) {
+	pt.x;
+	pt.y;
+}
+{
+	auto n = points.size();
+	if (n < 3)
+		return false;
+
+	auto X_prod = [](T O, T A, T B) { return (A.x - O.x) * (B.y - O.y) - (A.y - O.y) * (B.x - O.x); };
+
+	int sign = 0;
+	for (auto i = 0u; i < points.size(); i++) {
+		auto O = points[i];
+		auto A = points[(i + 1) % n];
+		auto B = points[(i + 2) % n];
+		auto cross = X_prod(O, A, B);
+		if (cross == 0)
+			continue;
+
+		if (sign == 0)
+			sign = cross;
+
+		if ((sign > 0 && cross < 0) || (sign < 0 && cross > 0))
+			return true;
+	}
+	return false;
+}
+
+namespace CpputilGeometry
+{
+
+struct TestPoint {
+	uint8_t x;
+	uint8_t y;
+};
+
+constexpr std::array<TestPoint, 4> tri{{{1, 1}, {2, 1}, {2, 2}, {1, 2}}};
+static_assert(is_poly_concave<TestPoint>(tri) == false);
+
+constexpr std::array<TestPoint, 5> cave{{{1, 1}, {2, 1}, {2, 0}, {2, 2}, {1, 2}}};
+static_assert(is_poly_concave<TestPoint>(cave) == true);
+
+} // namespace CpputilGeometry
