07436e211c96428b92ef33b68e88037dd1071378
Dan Green <danngreen1@gmail.com>
2025-04-14 09:16:11 -0700

    More callable tests

--- tests/callable_tests.cc
+++ tests/callable_tests.cc
@@ -13,3 +13,38 @@ TEST_CASE("Callable Function") {
 	CHECK(func(10, 10) == 40);
 	CHECK(capture == 20);
 }
+
+TEST_CASE("Callable Function mixed params") {
+	auto capture{0};
+	auto func = Function<int(int, float)>{[&capture](int a, float b) {
+		capture += 10;
+		return a + b + capture;
+	}};
+
+	CHECK(func(10, 10.f) == 30);
+	CHECK(capture == 10);
+	CHECK(func(10, 10.f) == 40);
+	CHECK(capture == 20);
+}
+
+TEST_CASE("Callable Function lots of params") {
+	struct A {
+		int a;
+		char b;
+		float d;
+		double e;
+	};
+	auto capture{0};
+	auto func = Function<int(int, float, char, A, A)>{[&capture](int a, float b, char x, A aa, A bb) {
+		capture += 10;
+		if (x == 'a')
+			return aa.a + bb.b + capture * 2.f;
+		else
+			return a + b + capture;
+	}};
+
+	CHECK(func(10, 10.f, 'x', {}, {2, 'b', 1.2f, 1.4}) == 30);
+	CHECK(capture == 10);
+	CHECK(func(10, 10.f, 'a', {}, {2, 'a', 1.2f, 1.4}) == 137);
+	CHECK(capture == 20);
+}
