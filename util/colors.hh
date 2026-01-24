630a8f9d46cf27753d4f98d84b1014bef8cab701
audiobird <cantrellhepner@gmail.com>
2026-01-24 10:19:26 -0800

    constexpr color operations

--- util/colors.hh
+++ util/colors.hh
@@ -51,15 +51,15 @@ struct Color {
 		return b_;
 	}
 
-	const Color operator+(Color const that) const {
+	constexpr Color operator+(Color const that) const {
 		return Color(builtin_add_u8(r_, that.r_), builtin_add_u8(g_, that.g_), builtin_add_u8(b_, that.b_));
 	}
 
-	const Color operator*(float phase) const {
+	constexpr Color operator*(float phase) const {
 		return Color{0, 0, 0}.blend(*this, phase);
 	}
 
-	bool operator==(Color const &other) const {
+	constexpr bool operator==(Color const &other) const {
 		return (r_ == other.r_ && g_ == other.g_ && b_ == other.b_);
 	}
 
