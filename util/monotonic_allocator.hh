365903bf86f325261ec90af238046b266639b83a
Dan Green <danngreen1@gmail.com>
2026-06-19 13:47:45 -0700

    MonotonicAllocator defaults alignment to 8 (can be overriden per allocation)

--- util/monotonic_allocator.hh
+++ util/monotonic_allocator.hh
@@ -1,18 +1,25 @@
 #pragma once
+#include <cstddef>
 #include <cstdint>
 #include <span>
 
 class MonotonicAllocator {
 public:
+	// All allocations are aligned up to at least this many bytes
+	static constexpr std::size_t DefaultAlign = 8;
+
+	static_assert((DefaultAlign & (DefaultAlign - 1)) == 0, "DefaultAlign must be a power of 2");
+	static_assert(DefaultAlign != 0, "DefaultAlign must be non-zero");
+
 	MonotonicAllocator(std::span<uint8_t> arena_)
 		: arena(arena_) {
 	}
 
-	uint8_t *allocate(std::size_t size) {
-		auto bytesRemaining = arena.size() - offset;
-		if (bytesRemaining > size) {
-			auto result = &arena[offset];
-			offset += size;
+	uint8_t *allocate(std::size_t size, std::size_t align = DefaultAlign) {
+		auto aligned_offset = (offset + (align - 1)) & ~(align - 1);
+		if (aligned_offset <= arena.size() && (arena.size() - aligned_offset) > size) {
+			auto result = &arena[aligned_offset];
+			offset = aligned_offset + size;
 
 			return result;
 		} else {
